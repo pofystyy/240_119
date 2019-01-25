36a1c65aaf462fa0038a2a37a6d0a04ffa10e6e4
pofystyy <pofystyy@gmail.com>
2019-01-25 13:30:32 +0200

    add model Education

---
+++ app/models/education.rb
@@ -0,0 +1,9 @@
+class Education
+  include Mongoid::Document
+  field :name,         type: String
+  field :launch_date,  type: String
+  field :description,  type: String
+  field :requirements, type: String
+  field :results,      type: String
+  field :active,       type: Boolean
+end
