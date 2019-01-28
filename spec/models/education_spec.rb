9d01b05c8e928ab98e54da97783eeead5c60b4f0
pofystyy <pofystyy@gmail.com>
2019-01-28 13:19:39 +0200

    changes for model Education

--- spec/models/education_spec.rb
+++ spec/models/education_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Education, type: :model do
   it "is valid with valid attributes" do
     expect(Education.new).to be_valid
   end
+
+  it "is valid with valid attributes" do
+    expect(Education.new.active).to eq true
+  end
+
+  it "is valid with valid attributes" do
+    education = Education.new(active: true)
+    expect(education.active).to eq true
+  end
+
+  it "is valid with valid attributes" do
+    education = Education.new(active: false)
+    expect(education.active).to eq false
+  end
+
+  it "is valid with valid attributes" do
+    education = Education.new(active: false)
+    expect(education.active).not_to eq true
+  end
 end
