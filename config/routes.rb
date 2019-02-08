6452b4a575dc5cae8944a0770416489de7ac914e
pofystyy <pofystyy@gmail.com>
2019-02-08 14:48:19 +0200

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   get '/sign_up', to: 'users#new', as: :sign_up
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/log_in', to: 'sessions#new', as: :log_in
+  get '/log_out', to: 'sessions#destroy', as: :log_out
 end
